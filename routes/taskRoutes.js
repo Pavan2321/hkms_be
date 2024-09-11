99e7621a3f97478c24988a7c217b8de22de85099
pradoshKumar <pradosh@zool.in>
2024-09-11 11:04:25 +0530

    Updated CORS error resolve

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { AuthUtil } = require('../utils/auth');
 
 router.post('/', createTask);
-router.get('/', AuthUtil.authenticate, getTasks);
+router.get('/', getTasks);
 router.get('/:id', getTaskById);
 router.put('/:id', updateTask);
 router.delete('/:id', deleteTask);
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const taskRoutes = require('./routes/taskRoutes');
-const authRoutes = require('./routes/authRoutes');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const taskRoutes = require("./routes/taskRoutes");
+const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
@@ -12,9 +13,12 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// CORS Middleware
+app.use(cors()); // This will allow access from any origin
+
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
